164cf30d27b1af8be814d3a7cde04428bf2b3d23
gabben12 <58600498+gabben12@users.noreply.github.com>
2019-12-10 10:01:28 -0500

    Ajouté interface

--- src/climatonix/Climatonix.java
+++ src/climatonix/Climatonix.java
@@ -15,7 +15,8 @@ public class Climatonix {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        InterfaceClimatonix weather = new InterfaceClimatonix();
+        weather.setVisible(true);
     }
     
 }

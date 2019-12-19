23adb3f62afc15157ed50ef7b6d4a47a4a65d38c
michaelzhang <michaelzhang608@gmail.com>
2019-12-19 13:10:07 -0500

    Added autocomplete feature

---
+++ src/climatonix/City.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package climatonix;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author michaelzhang
+ */
+public class City {
+
+    String id;
+    String name;
+    String country;
+    double lat;
+    double lon;
+
+    City(String[] data) {
+        try {
+            id = data[0];
+            name = data[1];
+            country = data[2];
+            lat = Double.parseDouble(data[3]);
+            lon = Double.parseDouble(data[4]);
+        } catch (Exception e) {
+        }
+    }
+}
--- src/climatonix/TestAutocomplete.java
+++ src/climatonix/TestAutocomplete.java
@@ -6,6 +6,7 @@
 package climatonix;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -13,14 +14,13 @@ import java.util.ArrayList;
  */
 public class TestAutocomplete extends javax.swing.JFrame {
 
+    AutocompleteUtil util;
     /**
      * Creates new form TestAutocomplete
      */
     public TestAutocomplete() {
-        
+        util = new AutocompleteUtil();
         initComponents();
-        autoComboBox = new Java2sAutoComboBox(new ArrayList());
-        autoTextField = new Java2sAutoTextField(new ArrayList(), autoComboBox);
         
     }
 
@@ -33,31 +33,90 @@ public class TestAutocomplete extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jComboBox1 = new javax.swing.JComboBox<>();
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        queryInput = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jButton1.setText("jButton1");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+
+        queryInput.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                queryInputActionPerformed(evt);
+            }
+        });
+        queryInput.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                queryInputKeyReleased(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(151, 151, 151)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
+                    .addComponent(queryInput, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(152, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(115, 115, 115)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(queryInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(112, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(153, Short.MAX_VALUE)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(151, 151, 151))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(102, 102, 102)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(171, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        
+        System.out.println(util.query(queryInput.getText()));
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void queryInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_queryInputActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_queryInputActionPerformed
+
+    private void queryInputKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_queryInputKeyReleased
+
+        String query = queryInput.getText();
+        System.out.println(query);
+        
+        if (query.isEmpty()) {
+            return;
+        }
+
+        if (query.toUpperCase().charAt(0) - 65 > 26 || query.toUpperCase().charAt(0) - 65 < 0) {
+            queryInput.setText("");
+            return;
+        }
+        
+        System.out.println(util.query(query));
+    }//GEN-LAST:event_queryInputKeyReleased
 
     /**
      * @param args the command line arguments
@@ -93,11 +152,10 @@ public class TestAutocomplete extends javax.swing.JFrame {
             }
         });
     }
-    
-    private Java2sAutoTextField autoTextField;
-    private Java2sAutoComboBox autoComboBox;
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField queryInput;
     // End of variables declaration//GEN-END:variables
 }

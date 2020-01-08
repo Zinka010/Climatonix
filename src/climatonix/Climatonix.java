8509618f4eec2e5bf04bf529f01145103677773c
Justin Toft <justintoft10@gmail.com>
2020-01-08 12:05:09 -0500

    minor changes

--- src/climatonix/Climatonix.java
+++ src/climatonix/Climatonix.java
@@ -19,7 +19,8 @@ public class Climatonix {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        InterfaceClimatonix interfaceClimatonix = new InterfaceClimatonix();
+        interfaceClimatonix.setVisible(true);
        
     }
 }
